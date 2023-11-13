29cd30082fcf82d2395a8a80318c07e3cae4bac4
austinventour <austin_ventour@hotmail.co.uk>
2023-11-13 16:40:42 +0000

    Initial React Vite project setup, About/Home & NavBar started

---
+++ MUA Portfolio/src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='About' className='w-full h-screen bg-slate-400 text-gray-900'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
+                <div>
+                    <p className='text-4xl font-cold inline border-b-4 border-pink-600'>International makeup artist Katie Moore specialises in enhancing natural beauty in a creative and playful way. Her expertise in working to any brief has allowed her to travel the world in collaboration with numerous clients and brands. Since acquiring a First Class BA Honours Degree in Makeup Artistry, Katie's work has been featured in the likes of Vogue, Elle, GQ and many more. Click here to contact Katie for makeup services. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ MUA Portfolio/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    
+    
+    <div name='Home' className='w-full h-screen bg-slate-300'>
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem veniam non sapiente minima possimus dignissimos, impedit cumque, esse saepe, temporibus quia odio eius ad velit laudantium cum consectetur excepturi sed.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
